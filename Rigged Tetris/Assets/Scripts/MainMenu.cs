13f39f913d181c03dda8ee6f0cde329aa76f6c81
Drake-Ironshield <43833916+Drake-Ironshield@users.noreply.github.com>
2022-01-31 12:42:24 -0800

    Continued Settings Menu

--- Rigged Tetris/Assets/Scripts/MainMenu.cs
+++ Rigged Tetris/Assets/Scripts/MainMenu.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
+
 
 public class MainMenu : MonoBehaviour
 {
-
+    public GameObject settingsMenu;
+    public int incrementAmount;
+    // Timing buttons
+    public GameObject sideText;
+    public int sideDefault;
+    static int sideValue;
+    public GameObject downText;
+    public int downDefault;
+    static int downValue;
+    public GameObject delayText;
+    public int delayDefault;
+    static int delayValue;
+    //Keybinds
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        settingsMenu.SetActive(false);
+        sideValue = sideDefault;
+        downValue = downDefault;
+        delayValue = delayDefault;
+        sideText.GetComponent<Text>().text = sideValue.ToString();
+        downText.GetComponent<Text>().text = downValue.ToString();
+        delayText.GetComponent<Text>().text = delayValue.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Exit()
@@ -31,6 +52,36 @@ public class MainMenu : MonoBehaviour
 
     public void openSettings()
     {
+        settingsMenu.SetActive(true);
+    }
 
+    public void closeSettings()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    public void increaseOrDecreaseValue(bool isIncreasing, string type)
+    {
+        int increment = incrementAmount;
+        if (!isIncreasing)
+        {
+            increment = increment * -1;
+        }
+        switch (type)
+        {
+            case "side":
+            if (sideValue + increment < 0 || sideValue + increment > 1000)
+            {
+                return;
+            }
+                sideValue = sideValue + increment;
+                sideText.GetComponent<Text>().text = sideValue.ToString();
+                break;
+            case "down":
+            case "delay":
+            default: 
+                Debug.Log("Something went wrong while incrementing");
+                break;
+        }
     }
 }
